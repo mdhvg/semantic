cfbeeaf3fbd5d9f5a4d40d7faf44a2e0ba5811a4
Madhav Goyal <madhavgyl1@gmail.com>
2025-01-11 21:48:18 +0530

    Cleanup: Remove pnpm-lock.yaml

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,23 +2,23 @@ import { defineConfig } from 'vitest/config'
 import path from 'path'
 
 export default defineConfig({
-  resolve: {
-    alias: {
-      $shared: path.resolve(__dirname, 'src/shared')
-    }
-  },
-  test: {
-    globals: true,
-    environment: 'node',
-    coverage: {
-      exclude: [
-        '!src/.{js,ts,cjs}',
-        'out/**/*',
-        'node_modules/**/*',
-        'resources/**/*',
-        'tests/**/*',
-        'dist/**/*'
-      ]
-    }
-  }
+	resolve: {
+		alias: {
+			$shared: path.resolve(__dirname, 'src/shared')
+		}
+	},
+	test: {
+		globals: true,
+		environment: 'node',
+		coverage: {
+			exclude: [
+				'!src/.{js,ts,cjs}',
+				'out/**/*',
+				'node_modules/**/*',
+				'resources/**/*',
+				'tests/**/*',
+				'dist/**/*'
+			]
+		}
+	}
 })
