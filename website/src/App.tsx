fab20691813d5bfe01e3c9587583fd006b986155
Madhav Goyal <madhavgyl1@gmail.com>
2025-03-02 04:17:27 +0530

    [website] Make navbar and tagline

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -5,16 +5,27 @@ import { NavButton } from './components/nav-button'
 function App() {
 	return (
 		<>
-			<div className="nav-container h-24 w-full text-foreground flex flex-row items-center">
-				<a className="nav-logo-container h-14 w-14 rounded-full bg-nav mx-5" href="/">
-					<Logo />
-				</a>
-				<div className="navbutton-container h-14 px-5 max-w-fit mx-auto flex justify-center gap-1 rounded-2xl bg-nav text-foreground">
-					<NavButton onClk={() => {}} children={'Home'} />
-					<NavButton onClk={() => {}} children={'Repo'} />
+			<nav className="nav fixed w-full max-h-fit">
+				<div className="nav-container max-w-fit max-h-fit mx-auto pl-5 pr-10 py-1 rounded-2xl text-foreground border border-solid border-border grid grid-cols-3 gap-1 place-content-cente backdrop-blur-sm">
+					<a className="nav-logo-container h-14 min-w-fit" href="/">
+						<Logo />
+					</a>
+					<div className="navbutton-container h-14 max-w-fit mx-auto grow flex justify-center gap-1 rounded-2xl text-foreground">
+						<NavButton onClk={() => {}} children={'Home'} />
+						<NavButton onClk={() => {}} children={'Features'} />
+						<NavButton onClk={() => {}} children={'Repo'} />
+					</div>
+					<div className="dummy w-14"></div>
 				</div>
-			</div>
-			<main className="text-foreground">It works</main>
+			</nav>
+			<main className="text-foreground mt-20 mx-auto sm:w-full md:w-5xl lg:w-7xl xl:w-7xl flex flex-col">
+				<div className="mx-auto pt-36 pb-5 text-7xl font-semibold font-space bigglow-highlight">
+					Search like you think
+				</div>
+				<div className="mx-auto pb-5 text-2xl font-space">
+					Collect everything you want, then find it when you need it.
+				</div>
+			</main>
 		</>
 	)
 }
--- website/src/components/Logo.tsx
+++ website/src/components/Logo.tsx
@@ -1,10 +1,10 @@
-import Semantic from '/1024x1024.png'
+import Semantic from '/Logo.png'
 
 export const Logo = () => {
 	return (
-		<div className="flex flex-row items-center gap-2 text-foreground text-2xl shadow-2xl">
-			<img src={Semantic} />
-			Semantic
+		<div className="flex flex-row items-center gap-2 text-foreground text-2xl">
+			<img src={Semantic} className="max-w-14" />
+			<div className="glow-highlight">Semantic</div>
 		</div>
 	)
 }
